7b033f55028bb2f37c90a41d7fdabb547167dc69
Melissa  Gall <melissa.gall@range-rp.com>
2025-06-04 13:40:14 -0500

    new style

--- map.js
+++ map.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWVsaXNzYWdhbGwyMDIzIiwiYSI6ImNsbWpzZmRkdTA1d
 
 const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/melissagall2023/cmbi8XXXXXXXXXX',
+  style: 'mapbox://styles/melissagall2023/cmbia5kao00ha01s161w58d9q',
   center: [-94.58295, 39.09187],
   zoom: 16.5,
   pitch: 70,
